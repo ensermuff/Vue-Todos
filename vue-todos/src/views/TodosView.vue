fb1179ea1cc398c1ca26f7d173dd9e7b5e626830
Firaol Ensermu <Firaolfayeensermu@gmail.com>
2023-05-24 22:31:08 -0400

    Continued to work on the TodoItem component and TodosView

--- vue-todos/src/views/TodosView.vue
+++ vue-todos/src/views/TodosView.vue
@@ -1,8 +1,9 @@
 <script setup>
-import { ref } from 'vue';
-import { uid } from 'uid';
-import TodoCreator from '../components/TodoCreator.vue';
-import TodoItem from '../components/TodoItem.vue';
+import { ref } from "vue";
+import { uid } from "uid";
+import { Icon } from "@iconify/vue";
+import TodoCreator from "../components/TodoCreator.vue";
+import TodoItem from "../components/TodoItem.vue";
 const todoList = ref([]);
 
 const createTodo = (todo) => {
@@ -13,30 +14,73 @@ const createTodo = (todo) => {
     isEditing: null,
   });
 };
+
+const toggleTodoComplete = (todoPos) => {
+  todoList.value[todoPos].isCompleted = !todoList.value[todoPos].isCompleted;
+}
+
+const toggleEditTodo = (todoPos) => {
+  todoList.value[todoPos].isEditing = !todoList.value[todoPos].isEditing;
+}
+
+const updateTodo = (todoVal, todoPos) => {
+  todoList.value[todoPos].todo = todoVal;
+}
+
+const deleteTodo = (todoId) => {
+  todoList.value = todoList.value.filter((todo) => todo.id !== todoId);
+}
+
 </script>
 
 <template>
   <main>
     <h1>Create Todo</h1>
-    <TodoCreator @create-todo="createTodo"/>
-    <ul>
-      <TodoItem v-for="todo in todoList" :todo="todo"/>
+    <TodoCreator @create-todo="createTodo" />
+    <ul class="todo-list" v-if="todoList.length > 0">
+      <TodoItem
+        v-for="(todo, index) in todoList"
+        :todo="todo"
+        :index="index"
+        @toggle-complete="toggleTodoComplete"
+        @edit-todo="toggleEditTodo"
+        @update-todo="updateTodo"
+        @delete-todo = "deleteTodo"
+      />
     </ul>
+    <p class="todos-msg" v-else>
+      <Icon icon="noto-v1:sad-but-relieved-face" width="22" />
+      <span>You have no todo's to complete! Add one! </span>
+    </p>
   </main>
 </template>
 
 <style lang="scss" scoped>
-  main {
+main {
+  display: flex;
+  flex-direction: column;
+  max-width: 500px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 40px 16px;
+
+  h1 {
+    margin-bottom: 16px;
+    text-align: center;
+  }
+  .todo-list {
     display: flex;
     flex-direction: column;
-    max-width: 500px;
-    width: 100%;
-    margin: 0 auto;
-    padding: 40px 16px;
-
-    h1 {
-      margin-bottom: 16px;
-      text-align: center;
-    }
+    list-style: none;
+    margin-top: 24px;
+    gap: 20px;
   }
+  .todos-msg {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+    margin-top: 24px;
+  }
+}
 </style>
